8e4cf27641ee1baa5dee377348177d5dab42d069
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2020-02-14 10:28:06 -0500

    probando el click en el boton

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+document.querySelector('button').addEventListener('click', () => {
+    console.log(`Hola! Mucho gusto en conocerte`);
+  });

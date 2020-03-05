c5c923fd0e5c6a2d77f5fc376e862938f609f7d6
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2020-03-05 11:54:35 -0500

    ultimos cambios

--- src/index.js
+++ src/index.js
@@ -19,62 +19,30 @@ const check = document.getElementById("ValidarCardNumber");
 check.addEventListener( "click" , () => {
   let x = validator.isValid(creditCard.value);
   if (x == true) {
-    alert(" Tarjeta Verificada  ✅ ");
+    alert(" Tarjeta Verificada ");
     let hiddenCard = validator.maskify(creditCard.value);
     document.getElementById('tarjetaOculta').innerHTML = " Tarjeta N° " + hiddenCard;
     document.getElementById('signup').classList.add("ocultar");
     document.getElementById('vista3').classList.remove("ocultar");
   } else {
-    alert(" ❌ Tarjeta NO Verificada ⚠️ ");
+    alert("Tarjeta NO Verificada");
   }
 
 });
 
 // Declarando variable para llamar al boton de Regresar de la vista 2 , para que me lleve a la vista 1
 
-const returnPage = document.getElementById("btnRegresar");
-returnPage.addEventListener("click", () => {
+const retornoPagina = document.getElementById("Regresar1");
+retornoPagina.addEventListener("click", () => {
 
 document.getElementById('signup').classList.add("ocultar");
 document.getElementById('login').classList.remove("ocultar");
 
 });
 
-// Declarando variable para llamar al boton de Pagar de la vista 3 y capturando el valor de mis otras vistas , para que me lleve a la vista 4 con todos datos capturados y salga mi comprobante de pago
-
-const pagar = document.getElementById("btnPagar");
-pagar.addEventListener("click", () => {
-
-let cardValue1 = validator.maskify(creditCard.value);
-let titu = document.getElementById('nameTitular').value;
-let code = document.getElementById('codigo').value;
-let mod = document.getElementById('modalidad');
-let mod1 = mod.options[mod.selectedIndex].text;
-let cor = document.getElementById('core');
-let cor1 = cor.options[cor.selectedIndex].text;
-let money = document.getElementById('monto');
-let money1 = money.options[money.selectedIndex].text;
-let mail = document.getElementById('email').value;
-
-
-document.getElementById('comprobanteTarjeta').innerHTML = " Tarjeta N° " + cardValue1;
-document.getElementById('comprobanteTitular').innerHTML = " Titular  : " + titu;
-document.getElementById('comprobanteCodigo').innerHTML = " Código   : " + code;
-document.getElementById('comprobanteModalidad').innerHTML = " Modalidad : " + mod1;
-document.getElementById('comprobanteCore').innerHTML = " Commo Core   : " + cor1;
-document.getElementById('comprobanteMonto').innerHTML = " Monto Pagado : " + money1;
-document.getElementById('comprobanteEmail').innerHTML = " Este comprobante será enviado a : " + mail;
-
-
-
-document.getElementById('vista3').classList.add("ocultar");
-document.getElementById('vista4').classList.remove("ocultar");
-
-});
-
 // Declarando variable para llamar al boton de Regresar2  de la vista 3 , para que me lleve a la vista 2
 
-const returnValidatorPage = document.getElementById("btnRegresar2");
+const returnValidatorPage = document.getElementById("Regresar2");
 returnValidatorPage.addEventListener("click", () => {
 
 document.getElementById('vista3').classList.add("ocultar");
--- src/validator.js
+++ src/validator.js
@@ -1,85 +1,22 @@
+
 const validator = {
-  isValid : (numCard)=> {
-
-  let reverseNumber = numCard.split("").reverse();
-
-// Separando los numeros de las posiciones Pares e Inpares.
-  const parNumber = [];
-  const inparNumber = [];
-  for (let i = 0; i < reverseNumber.length; i=i+1){
-    if (i%2==1) {
-      parNumber.push(parseInt(reverseNumber[i]*2)); // multiplicando por 2 a los numeros de las posiciones Inpares -> Originalmente son Pares
-    }else {
-      inparNumber.push(parseInt(reverseNumber[i])); // extrayendo los numeros pares -> Originalmente son Inpares
+  isValid: (numCard) => {
+    const creditNumber = numCard.split('').reverse(); 
+    for (let i = 0; i < creditNumber.length; i++) {
+        if (i % 2 == 1) {
+            creditNumber[i] = creditNumber[i] * 2;
+        }
     }
-  }
-
-  const parNumberTwoDigit = [];
-  for (let i = 0; i < parNumber.length; i++) {
-    if(parNumber[i]>=10){
-      parNumberTwoDigit.push(parNumber[i]);
+    let newCreditNumber = creditNumber.join('').split('');
+    let counter = 0;
+    for (let i = 0; i < newCreditNumber.length; i++) {
+        counter += parseInt(newCreditNumber[i]);
     }
-  }
-
-  const parNumberOneDigit = [];
-  for (let i = 0; i < parNumber.length; i++) {
-    if(parNumber[i]<=9){
-      parNumberOneDigit.push(parNumber[i]);
+    if (counter % 10 == 0) {
+        return true;
+    } else {
+        return false;
     }
-  }
-
-  const sumatoriaParNumber=[];
-  let contador1 = 0;
-  for (let i = 0; i < parNumberOneDigit.length; i++) {
-    contador1 += parNumberOneDigit[i];
-  }
-
-  sumatoriaParNumber.push(contador1);
-
-  const sumatoriaInparNumber=[];
-  let contador2 = 0;
-  for (let i = 0; i < inparNumber.length; i++) {
-    contador2 += inparNumber[i];
-  }
-  sumatoriaInparNumber.push(contador2);
-
-  let changeOneDigit = parNumberTwoDigit.join("").split("");
-  const changeArrayNumber = [];
-  for (let i = 0; i < changeOneDigit.length; i=i+1){
-    if (i<=16) {
-      changeArrayNumber.push(parseInt(changeOneDigit[i]));
-    }
-  }
-
-  const sumatoriaParTwoDigit=[];
-  let contador3 = 0;
-  for (let i = 0; i < changeArrayNumber.length; i++) {
-    contador3 += changeArrayNumber[i];
-  }
-
-  sumatoriaParTwoDigit.push(contador3);
-
- const array1 = sumatoriaParNumber;
- const array2 = sumatoriaInparNumber;
- const array3 = sumatoriaParTwoDigit;
- const array4 = array1.concat(array2,array3);
-
- const sumatoriaTotal=[];
-
- let contador4 = 0;
- for (let i = 0; i < array4.length; i++) {
-   contador4 += array4[i];
- }
-
- sumatoriaTotal.push(contador4);
-
- for (let i = 0; i < sumatoriaTotal.length; i++){
-     if(sumatoriaTotal[i]%10 == 0){
-       return true;
-     } else {
-       return false;
-     }
- }
 },
 
 maskify:(numCard) => {
